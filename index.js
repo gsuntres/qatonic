4633fb87ca8a2312a78b3f6309121ef67b17e08d
George Suntres <gsuntres@gmail.com>
2019-06-18 08:53:47 -0400

    Add set/get fixtures.

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ class QaTonic {
       slow: 1000
     })
     this._context = new Context()
+    this._fixtures = {}
   }
 
   get mocha() {
@@ -23,6 +24,21 @@ class QaTonic {
 
   get v() {
     return this._context.ctx
+  }
+
+  fixture(key, obj) {
+    if(typeof key !== 'string') {
+      throw new Error('key should be a string')
+    }
+
+    if(typeof obj === 'undefined') {
+      return this._fixtures[key]
+    } else if(typeof obj === 'null' && typeof this._fixtures[key] !== 'undefined') {
+      delete this._fixtures[key]
+    } else {
+      this._fixtures[key] = Object.assign({}, obj)
+      Object.freeze(this._fixtures[key])
+    }
   }
 
   loadFiles() {
